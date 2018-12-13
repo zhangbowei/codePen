3bb5854828f834d429a1e0cff53af0c1101ad2bf
张博伟 <zhangbowei02@meituan.com>
2018-12-13 12:02:28 +0800

    test

--- example/js/formData.js
+++ example/js/formData.js
@@ -1,10 +1,12 @@
-$('button').click(function () {
-    const domArr = $('#test-form [name]');
-    const rawArr = Array.prototype.slice.call(domArr);
+$('button').click(function() {
+    const domArr = $('#test-form [name]')
+    const rawArr = Array.prototype.slice.call(domArr)
 
-    const res = rawArr.reduce(function (prev, item) {
-        return (item.type !== "radio" || item.checked) ? prev.concat([{ [item.name]: item.value }]) : prev;
-    }, []);
+    const res = rawArr.reduce(function(prev, item) {
+        return item.type !== 'radio' || item.checked
+            ? prev.concat([{ [item.name]: item.value }])
+            : prev
+    }, [])
 
-    console.log(JSON.stringify(res));
-});
+    console.log(JSON.stringify(res))
+})
